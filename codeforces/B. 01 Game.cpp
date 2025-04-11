6548b6ac4f7de6a5056afcd2d74dd6962704b606
Tanuj-A06 <tanuj.adarkar06@gmail.com>
2025-04-11 20:15:40 +0530

    Added Documentation to the code, explaning the methodology used.

--- codeforces/B. 01 Game.cpp
+++ codeforces/B. 01 Game.cpp
@@ -20,3 +20,11 @@ int main(){
         solve();
     }
 }
+
+/**
+ * DOCUMENTATION:
+ * Count the number of 1's and 0's and find the min of those, since number of operations
+ * can't exceed the min.
+ * Since Alice's turn is odd and not ending on her will make us loose,
+ * so if number of operstions is even print NET(no) else DA(yes)
+ */
